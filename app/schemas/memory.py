33def5369a83020066701187be38997e8742bedc
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-10 14:07:47 +0530

    added uuid support

--- app/schemas/memory.py
+++ app/schemas/memory.py
@@ -1,10 +1,11 @@
 from pydantic import BaseModel
+from uuid import UUID
 
 class MemoryCreate(BaseModel):
     memory: str
     type: str
     
 class MemoryResponse(BaseModel):
-    id: int
+    id: UUID
     memory: str
     type: str
