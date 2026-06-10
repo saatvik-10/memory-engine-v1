ed60159bf57215baebb9ab6d22da4f15b5c6c6dd
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-10 14:59:17 +0530

    added get with id route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from uuid import uuid4
+from uuid import uuid4, UUID
+from fastapi.exceptions import HTTPException
 from app.schemas.memory import MemoryCreate
 from app.schemas.memory import MemoryResponse
 
@@ -7,7 +8,7 @@ app = FastAPI()
 
 fake_memory_db: list[MemoryResponse] = []
 
-@app.post('/memory', response_model = MemoryResponse)
+@app.post('/memory', response_model=MemoryResponse)
 def create_memory(memory: MemoryCreate):
     stored_memory = MemoryResponse(
         id=uuid4(),
@@ -16,7 +17,14 @@ def create_memory(memory: MemoryCreate):
     )
     fake_memory_db.append(stored_memory)
     return stored_memory
-    
-@app.get('/memory')
+
+@app.get('/memory', response_model=list[MemoryResponse])
 def get_memory() -> list[MemoryResponse]:
     return fake_memory_db
+
+@app.get('/memory/{id}', response_model=MemoryResponse)
+def get_memory_by_id(id: UUID):
+    for memory in fake_memory_db:
+        if memory.id == id:
+            return memory
+    raise HTTPException(status_code=404, detail="Memory not found")
