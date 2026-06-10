a0c14084390ed0dd1aa34efb29911aafa283b9e8
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-10 14:07:55 +0530

    added memory get route

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from uuid import uuid4
 from app.schemas.memory import MemoryCreate
+from app.schemas.memory import MemoryResponse
 
 app = FastAPI()
 
-@app.post('/memory')
+fake_memory_db: list[MemoryResponse] = []
+
+@app.post('/memory', response_model = MemoryResponse)
 def create_memory(memory: MemoryCreate):
-    return {
-        "message": "memory received",
-        "memory": memory
-    }
+    stored_memory = MemoryResponse(
+        id=uuid4(),
+        memory=memory.memory,
+        type=memory.type,
+    )
+    fake_memory_db.append(stored_memory)
+    return stored_memory
+    
+@app.get('/memory')
+def get_memory() -> list[MemoryResponse]:
+    return fake_memory_db
