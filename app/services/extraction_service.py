98a95bcc2b5b1ea8636f652ba0b4f9c6df6e4760
saatvik-10 <saatvikmadan529@gmail.com>
2026-06-12 17:00:32 +0530

    added temp raw data extractor

---
+++ app/services/extraction_service.py
@@ -0,0 +1,22 @@
+MEMORY_KEYWORDS = [
+    "learning",
+    "interested",
+    "working",
+    "building",
+    "prefer",
+    "hate",
+    "love",
+    "goal",
+    "want",
+    "plan",
+    "exploring",
+    "studying"
+]
+
+def should_extract(text: str) -> bool:
+    text = text.lower()
+    
+    return any(
+        keyword in text
+        for keyword in MEMORY_KEYWORDS
+    )
